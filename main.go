81829ca468172ac666425a09e78de456cb6539fd
Bryjamin <benjaminabryante@hotmail.co.uk>
2018-09-08 21:26:49 +0100

    Created a 'World' which tracks map positions

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"sync"
 	"time"
 
@@ -12,15 +13,94 @@ import (
 
 const hungerPerMoment = 2
 
+type MapPoint struct {
+	Gopher Gopher
+	Food   food.Food
+}
+
+func (mp *MapPoint) isEmpty() bool {
+	return mp.Gopher == Gopher{} && mp.Food == food.Food{}
+}
+
+type World struct {
+	world map[string]MapPoint
+
+	width  int
+	height int
+
+	inputActions chan func()
+	outputAction chan func()
+
+	activeGophers chan *Gopher
+}
+
+func (world *World) SetUpMapPoints(numberOfGophers int, numberOfFood int) {
+
+	keys := make([]string, len(world.world))
+
+	i := 0
+	for k := range world.world {
+		keys[i] = k
+		i++
+	}
+
+	rand.Shuffle(len(keys), func(i, j int) {
+		keys[i], keys[j] = keys[j], keys[i]
+	})
+
+	fmt.Println(len(keys))
+	count := 0
+
+	world.activeGophers = make(chan *Gopher, numberOfGophers)
+
+	for i := 0; i < numberOfGophers; i++ {
+		var mapPoint = world.world[keys[count]]
+		mapPoint.Gopher = newGopher(strconv.Itoa(i), maputil.StringToCoordinates(keys[count]))
+		world.activeGophers <- &mapPoint.Gopher
+		world.world[keys[count]] = mapPoint
+	}
+
+	for i := 0; i < numberOfFood; i++ {
+		var mapPoint = world.world[keys[count]]
+		mapPoint.Food = food.NewPotato()
+		world.world[keys[count]] = mapPoint
+	}
+
+}
+
+func CreateWorld(width int, height int) World {
+
+	world := World{width: width, height: height}
+	world.inputActions = make(chan func(), 10000)
+	world.outputAction = make(chan func(), 10000)
+
+	world.world = make(map[string]MapPoint)
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			world.world[maputil.CoordinateMapKey(x, y)] = MapPoint{}
+		}
+	}
+
+	world.inputActions <- func() {
+		fmt.Println("Hello")
+	}
+
+	world.SetUpMapPoints(10000, 100)
+
+	return world
+
+}
+
 type Gopher struct {
 	name     string
 	lifespan int
 	hunger   int
 
-	position *maputil.Coordinates
+	position maputil.Coordinates
 }
 
-func newGopher(name string, coord *maputil.Coordinates) Gopher {
+func newGopher(name string, coord maputil.Coordinates) Gopher {
 	return Gopher{name: name, lifespan: 0, hunger: rand.Intn(1000), position: coord}
 }
 
@@ -36,6 +116,10 @@ func (g *Gopher) applyHunger() {
 	g.hunger -= hungerPerMoment
 }
 
+func (g *Gopher) Move(x int, y int) {
+
+}
+
 func (g *Gopher) Eat() {
 
 	//for i := 0; i < 100; i++ {
@@ -48,14 +132,13 @@ func (g *Gopher) Dig() {
 	time.Sleep(100)
 }
 
-func CreateMap(width int, height int) map[string]struct{} {
+func CreateMap(width int, height int) map[string]*Gopher {
 
-	var m = make(map[string]struct{})
+	var m = make(map[string]*Gopher)
 
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
-			var s struct{}
-			m[maputil.CoordinateMapKey(x, y)] = s
+			m[maputil.CoordinateMapKey(x, y)] = &Gopher{}
 		}
 	}
 
@@ -81,11 +164,25 @@ func PerformMoment(wg *sync.WaitGroup, g *Gopher, c chan *Gopher) {
 
 }
 
-func generateGopher(inputChannel chan *Gopher, start *maputil.Coordinates) {
-	goph := newGopher(fmt.Sprintf("[1]", rand.Int()), start)
+func generateGopher(inputChannel chan *Gopher, coordinates string, world map[string]*Gopher) {
+	goph := newGopher(fmt.Sprintf("[1]", rand.Int()), maputil.StringToCoordinates(coordinates))
 
 	select {
 	case inputChannel <- &goph:
+		//world[coordinates] = goph
+	}
+
+}
+
+func RenderMap(myMap map[string]*Gopher) {
+
+	for k := range myMap {
+
+		var s = fmt.Sprint(myMap[k])
+		if s != "&{ 0 0 {0 0}}" {
+			fmt.Println(s)
+		}
+
 	}
 
 }
@@ -95,42 +192,20 @@ func main() {
 	//	runtime.GOMAXPROCS(1)
 	start := time.Now()
 
-	rand.Perm(1000)
+	width, height := 1000, 1000
 
-	width := 1000
-	height := 1000
+	var world = CreateWorld(width, height)
+	fmt.Println(world)
 
 	var mymap = CreateMap(width, height)
-
-	keys := make([]string, len(mymap))
-
-	i := 0
-	for k := range mymap {
-		keys[i] = k
-		i++
-	}
-
-	rand.Shuffle(len(keys), func(i, j int) {
-		keys[i], keys[j] = keys[j], keys[i]
-	})
-
-	//fmt.Print(keys)
 
 	var wg sync.WaitGroup
 
 	var numGophers = 10000
 
-	channel := make(chan *Gopher, numGophers)
+	var channel = world.activeGophers
 
-	count := 0
-
-	for len(channel) != cap(channel) {
-		var c = maputil.StringToCoordinates(keys[count])
-		go generateGopher(channel, &c)
-		count++
-	}
-
-	i = 0
+	i := 0
 
 	for numGophers > 0 {
 
@@ -139,6 +214,8 @@ func main() {
 		secondChannel := make(chan *Gopher, numGophers)
 		for i := 0; i < numGophers; i++ {
 			msg := <-channel
+
+			mymap[msg.position.MapKey()] = msg
 			wg.Add(1)
 
 			go PerformMoment(&wg, msg, secondChannel)
@@ -147,6 +224,8 @@ func main() {
 		i++
 		wg.Wait()
 	}
+
+	RenderMap(mymap)
 
 	fmt.Println(time.Since(start))
 	fmt.Println("Done")
