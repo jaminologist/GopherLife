64d4458c03c42c75c2051338baef114811130520
Bryjamin <benjaminabryante@hotmail.co.uk>
2018-09-23 15:36:48 +0100

    Added a world render that displays the world in a hrml file

--- main.go
+++ main.go
@@ -2,50 +2,46 @@ package main
 
 import (
 	"fmt"
-	food "gopherlife/food"
 	gopherlife "gopherlife/world"
-	"os"
-	"strconv"
+	"net/http"
 	"time"
 )
 
 const numberOfGophs = 5
 
-const size = 30
+const size = 1000
 
 func main() {
 
-	//	runtime.GOMAXPROCS(1)
+	//runtime.GOMAXPROCS(1)
 	start := time.Now()
 
-	width, height := size, size
-	var world = gopherlife.CreateWorld(width, height)
+	var world = gopherlife.CreateWorld()
 
 	for len(world.ActiveGophers) > 0 {
+		world.ProcessWorld()
+		//mux := http.NewServeMux()
 
-		fmt.Println("Enter Command...")
-		var input, value string
-		fmt.Scanln(&input, &value)
-
-		switch input {
-		case "at", "advancetime":
-			time, err := strconv.Atoi(value)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(2)
-			}
-
-			for time > 0 {
-				world.ProcessWorld()
-				time--
-			}
-
-		}
+		http.HandleFunc("/", worldToHTML(&world))
+		//mux.HandleFunc("/", worldToHTML(&world))
+		http.ListenAndServe(":8080", nil)
 	}
 
 	fmt.Println(time.Since(start))
 	fmt.Println("The World Lasted for ", world.Moments, " Moments")
 	fmt.Println("Done")
+}
 
-	fmt.Println(food.NewCarrot().Name)
+func worldToHTML(world *gopherlife.World) func(w http.ResponseWriter, r *http.Request) {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, world.RenderWorld())
+	}
+
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<p> Hello </p>")
+	fmt.Fprintln(w, "<p> Hello  Again</p>")
+	fmt.Fprintln(w, "<p> Hello WURTU Again</p>")
 }
