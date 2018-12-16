4b6b4f8519f7b5c9c21584b915a3775746ba338f
Bryjamin <benjaminabryante@hotmail.co.uk>
2018-12-16 19:59:22 +0000

    Made changes to fix spiral method by converting to use floats

--- calc/SpiralSearch.go
+++ calc/SpiralSearch.go
@@ -1,41 +1,24 @@
 package calc
 
 import (
-	"fmt"
 	"math"
 )
-
-func SpiralCoordinates(n int) {
-
-	x, y, d, m := 0, 0, 1, 1
-
-	for i := 0; i < n; i++ {
-		for (2 * x * d) < m {
-			fmt.Println(x, y)
-			x = x + d
-		}
-		for (2 * y * d) < m {
-			fmt.Println(x, y)
-			y = y + d
-		}
-
-		d = -1 * d
-		m = m + 1
-	}
-
-}
 
 type Spiral struct {
 	width  int
 	height int
 
-	x int
-	y int
+	x float64
+	y float64
 
-	dx int
-	dy int
+	bigX float64
+	bigY float64
 
-	i int
+	dx float64
+	dy float64
+
+	i    int
+	maxI int
 }
 
 func NewSpiral(width int, height int) Spiral {
@@ -43,26 +26,29 @@ func NewSpiral(width int, height int) Spiral {
 	return Spiral{
 		width:  width,
 		height: height,
+		bigX:   float64(width) / 2,
+		bigY:   float64(height) / 2,
 		x:      0,
 		y:      0,
 		dx:     0,
 		dy:     -1,
 		i:      0,
+		maxI:   int(math.Pow(float64(Max(width, height)), 2)),
 	}
 
 }
 
 func (s *Spiral) Next() (Coordinates, bool) {
 
-	X := s.width
-	Y := s.height
+	var coords = Coordinates{-1, -1}
+	var foundCoords = false
 
-	var coords = Coordinates{}
+	for s.i < s.maxI {
 
-	for s.i < int(math.Pow(float64(Max(X, Y)), 2)) {
-
-		if (-X/2 < s.x) && (s.x <= X/2) && (-Y/2 < s.y) && (s.y <= Y/2) {
-			coords.X, coords.Y = s.x, s.y
+		if ((-s.bigX < s.x) && (s.x <= s.bigX)) &&
+			((-s.bigY < s.y) && (s.y <= s.bigY)) {
+			coords.X, coords.Y = int(s.x), int(s.y)
+			foundCoords = true
 		}
 
 		if (s.x == s.y) || (s.x < 0 && s.x == -s.y) || (s.x > 0 && s.x == 1-s.y) {
@@ -73,41 +59,15 @@ func (s *Spiral) Next() (Coordinates, bool) {
 
 		s.i = s.i + 1
 
-		if coords != (Coordinates{}) {
+		if foundCoords {
 			break
 		}
 
 	}
 
-	if coords == (Coordinates{}) {
+	if !foundCoords {
 		return coords, false
 	}
 
 	return coords, true
-}
-
-func SpiralCoordinates2(n int) {
-
-	X, Y := 20, 5
-
-	x, y := 0, 0
-
-	dx := 0
-	dy := -1
-
-	for i := 0; i < int(math.Pow(float64(Max(X, Y)), 2)); i++ {
-
-		if (-X/2 < x) && (x <= X/2) && (-Y/2 < y) && (y <= Y/2) {
-			fmt.Println(x, y)
-		}
-
-		if (x == y) || (x < 0 && x == -y) || (x > 0 && x == 1-y) {
-			dx, dy = -dy, dx
-		}
-
-		x, y = x+dx, y+dy
-	}
-
-	//return x, y
-
 }
