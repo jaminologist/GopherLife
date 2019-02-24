ee5340622a3478e15b8ee506f3fc9475a1e99ac3
Bryjamin <benjaminabryante@hotmail.co.uk>
2019-02-24 15:34:28 +0000

    Added more page variables

--- handlers/worldhandler.go
+++ handlers/worldhandler.go
@@ -20,15 +20,12 @@ type UpdateableRender interface {
 type container struct {
 	tileMap  UpdateableRender
 	tileMaps map[string]func(world.Statistics) UpdateableRender
+	data     WorldPageData
 }
 
 type SelectReturn struct {
 	WorldRender string
 	Gopher      *world.Gopher
-}
-
-type PageVariables struct {
-	Data template.HTML
 }
 
 func SetUpPage() {
@@ -42,38 +39,44 @@ func SetUpPage() {
 		GopherBirthRate:        7,
 	}
 
-	/*stats := world.Statistics{
-		Width:                  3000,
-		Height:                 3000,
-		NumberOfGophers:        5000,
-		NumberOfFood:           1000000,
-		MaximumNumberOfGophers: 1000000,
-		GopherBirthRate:        7,
-	}*/
+	data := WorldPageData{
+		PageTitle: "G O P H E R L I F E",
+		FormData:  GetFormDataFromStats(stats),
+		MapData:   []MapData{},
+	}
 
 	var tileMap = world.NewGopherMapWithSpiralSearch(stats)
 
 	tileMapFunctions := make(map[string]func(world.Statistics) UpdateableRender)
-	tileMapFunctions["a"] = func(s world.Statistics) UpdateableRender {
+	tileMapFunctions["GopherMap With Spiral Search"] = func(s world.Statistics) UpdateableRender {
 		d := world.NewGopherMapWithSpiralSearch(s)
 		return &d
 	}
-	tileMapFunctions["b"] = func(s world.Statistics) UpdateableRender {
+	tileMapFunctions["GopherMap With Partition"] = func(s world.Statistics) UpdateableRender {
 		d := world.NewGopherMapWithParitionGridAndSearch(s)
 		return &d
 	}
-	tileMapFunctions["c"] = func(s world.Statistics) UpdateableRender {
+	tileMapFunctions["Cool Black And White Spiral"] = func(s world.Statistics) UpdateableRender {
 		d := world.NewSpiralMapController(s)
 		return &d
 	}
+
+	for k := range tileMapFunctions {
+		data.MapData = append(data.MapData, MapData{
+			DisplayName: k,
+			Value:       k,
+		})
+	}
+
 	container := container{
 		tileMap:  &tileMap,
 		tileMaps: tileMapFunctions,
+		data:     data,
 	}
 
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	http.HandleFunc("/", worldToHTML(&container))
+	http.HandleFunc("/", worldToHTML(&container, data))
 	http.HandleFunc("/ProcessWorld", ajaxProcessWorld(&container))
 	http.HandleFunc("/ShiftWorldView", ajaxHandleWorldInput(&container))
 	http.HandleFunc("/SelectGopher", ajaxSelectGopher(&container))
@@ -83,22 +86,82 @@ func SetUpPage() {
 
 }
 
-type WorldPageData struct {
-	PageTitle string
+func GetFormDataFromStats(stats world.Statistics) []FormData {
+
+	return []FormData{
+		FormData{
+			DisplayName:        "Width",
+			Type:               "Number",
+			Name:               "width",
+			Value:              strconv.Itoa(stats.Width),
+			BootStrapFormWidth: 1,
+		},
+		FormData{
+			DisplayName:        "Height",
+			Type:               "Number",
+			Name:               "height",
+			Value:              strconv.Itoa(stats.Height),
+			BootStrapFormWidth: 1,
+		},
+		FormData{
+			DisplayName:        "Initial Population",
+			Type:               "Number",
+			Name:               "numberOfGophers",
+			Value:              strconv.Itoa(stats.NumberOfGophers),
+			BootStrapFormWidth: 2,
+		},
+		FormData{
+			DisplayName:        "Max Population",
+			Type:               "Number",
+			Name:               "maxPopulation",
+			Value:              strconv.Itoa(stats.MaximumNumberOfGophers),
+			BootStrapFormWidth: 2,
+		},
+		FormData{
+			DisplayName:        "Birth Rate",
+			Type:               "Number",
+			Name:               "birthRate",
+			Value:              strconv.Itoa(stats.GopherBirthRate),
+			BootStrapFormWidth: 2,
+		},
+		FormData{
+			DisplayName:        "Food",
+			Type:               "Number",
+			Name:               "numberOfFood",
+			Value:              strconv.Itoa(stats.NumberOfFood),
+			BootStrapFormWidth: 2,
+		},
+	}
+
 }
 
-func worldToHTML(container *container) func(w http.ResponseWriter, r *http.Request) {
+type WorldPageData struct {
+	PageTitle string
+	FormData  []FormData
+	MapData   []MapData
+}
+
+type FormData struct {
+	DisplayName        string
+	Name               string
+	Value              string
+	Type               string
+	BootStrapFormWidth int
+}
+
+type MapData struct {
+	DisplayName string
+	Value       string
+}
+
+func worldToHTML(container *container, data WorldPageData) func(w http.ResponseWriter, r *http.Request) {
 
 	//	renderer := gopherlife.NewRenderer()
 
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		data := WorldPageData{
-			PageTitle: "G O P H E R L I F E",
-		}
-
 		tmpl := template.Must(template.ParseFiles("static/index.html"))
-		err := tmpl.Execute(w, data)
+		err := tmpl.Execute(w, container.data)
 
 		if err != nil {
 			log.Printf("Template executing error: ", err)
@@ -158,6 +221,8 @@ func resetWorld(container *container) func(w http.ResponseWriter, r *http.Reques
 		}
 
 		container.tileMap = tileMap
+		container.data.FormData = GetFormDataFromStats(stats)
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
 
